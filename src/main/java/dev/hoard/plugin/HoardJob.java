223ffef91c709d7f7dd7d93dfaefb60f8c93e319
lowacaop-prog <lowacaop@gmail.com>
2026-06-24 09:11:28 +0000

    working version

--- src/main/java/dev/hoard/plugin/HoardJob.java
+++ src/main/java/dev/hoard/plugin/HoardJob.java
@@ -4,17 +4,16 @@ import org.bukkit.Material;
 import java.util.*;
 public enum HoardJob {
     MINER("Miner", ChatColor.GRAY, Material.DIAMOND_PICKAXE, new Material[]{
-        Material.COAL_ORE, Material.DEEPSLATE_COAL_ORE,
-        Material.IRON_ORE, Material.DEEPSLATE_IRON_ORE,
-        Material.GOLD_ORE, Material.DEEPSLATE_GOLD_ORE, Material.NETHER_GOLD_ORE,
-        Material.DIAMOND_ORE, Material.DEEPSLATE_DIAMOND_ORE,
-        Material.EMERALD_ORE, Material.DEEPSLATE_EMERALD_ORE,
-        Material.LAPIS_ORE, Material.DEEPSLATE_LAPIS_ORE,
-        Material.REDSTONE_ORE, Material.DEEPSLATE_REDSTONE_ORE,
-        Material.COPPER_ORE, Material.DEEPSLATE_COPPER_ORE,
+        Material.COAL_ORE,
+        Material.IRON_ORE,
+        Material.GOLD_ORE,
+        Material.DIAMOND_ORE,
+        Material.EMERALD_ORE,
+        Material.LAPIS_ORE,
+        Material.REDSTONE_ORE,
+        Material.COPPER_ORE,
         Material.NETHER_QUARTZ_ORE, Material.ANCIENT_DEBRIS,
-        Material.STONE, Material.COBBLESTONE, Material.DEEPSLATE,
-        Material.COBBLED_DEEPSLATE, Material.GRANITE, Material.DIORITE,
+        Material.STONE, Material.DEEPSLATE, Material.GRANITE, Material.DIORITE,
         Material.ANDESITE, Material.OBSIDIAN, Material.NETHERRACK,
         Material.ICE, Material.PACKED_ICE, Material.BLUE_ICE
     }),
